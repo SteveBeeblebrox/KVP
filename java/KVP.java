5ab589919a2bd6c9cdaf30f0aa5f09497581fb42
SteveBeeblebrox <stevebeeblebrox@users.noreply.github.com>
2021-02-02 15:12:22 -0700

    Added getters

--- java/KVP.java
+++ java/KVP.java
@@ -34,7 +34,44 @@ public class KVP {
 			.replaceAll("([$!:'\"])", "\\$$1")
 			.replaceAll("\n", "$endline");
 	}
-	//TODO ...
+	public String getString(String key) { return this.getString(key, ""); }
+	public String getString(String key, String fallback) {
+		return this.<String>internalGetHelper(v -> v, (t, v) -> true, key, fallback);
+	}
+	public Boolean getBoolean(String key) { return this.getBoolean(key, Boolean.FALSE); }
+	public Boolean getBoolean(String key, Boolean fallback) {
+		return this.<Boolean>internalGetHelper(v -> v.trim().equalsIgnoreCase("true"), (t, v) -> v.trim().equalsIgnoreCase("true") || v.trim().equalsIgnoreCase("false"), key, fallback);
+	}
+	public Integer getInteger(String key) { return this.getInteger(key, new Integer(0)); }
+	public Integer getInteger(String key, Integer fallback) {
+		return this.<Integer>internalGetHelper(v -> {
+			try {
+				return Integer.valueOf(v.trim());
+			} catch(NumberFormatException exception) {
+				return null;
+			}
+		}, (t, v) -> t != null, key, fallback);
+	}
+	public Float getDecimal(String key) { return this.getDecimal(key, new Float(0)); }
+	public Float getDecimal(String key, Float fallback) {
+		return this.<Float>internalGetHelper(v -> {
+			try {
+				return Float.valueOf(v.trim());
+			} catch(NumberFormatException exception) {
+				return null;
+			}
+		}, (t, v) -> t != null, key, fallback);
+	}
+	public java.util.Date getDate(String key) { return this.getDate(key, new java.util.Date("January 1, 1970")); }
+	public java.util.Date getDate(String key, java.util.Date fallback) {
+		return this.<java.util.Date>internalGetHelper(v -> {
+			try {
+				return new java.util.Date(v.trim());
+			} catch(IllegalArgumentException exception) {
+				return null;
+			}
+		}, (t, v) -> t != null, key, fallback);
+	}
 	public boolean hasKey(String key) {
 		return this.pairs.containsKey(key);
 	}
